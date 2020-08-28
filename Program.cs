a376ac2e83b90bfd3430e665ed8888471aad3bf9
1ciko1 <57154845+1ciko1@users.noreply.github.com>
2020-08-28 13:54:43 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleProjectSix
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Random numberGen = new Random();
+
+            int numberOfAttempts = 0;
+            int attempt = 0;
+            
+            while (attempt != 6) //Is not equal to 6
+            {
+                attempt = numberGen.Next(1, 7);
+                Console.WriteLine("Tom rolled:" + attempt + ".");
+                numberOfAttempts++;
+            }
+            Console.WriteLine("It took Tom" + numberOfAttempts + "attempt to roll a six");
+            Console.ReadKey();
+        }
+    }
+}
